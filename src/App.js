f8e7acbee337f9140bc3ebb8f5e255dcdd15c980
Matthias Elliott <komizutama@gmail.com>
2018-10-03 01:38:00 -0400

    added FAQ and a few extra styling notes..

--- src/App.js
+++ src/App.js
@@ -1,34 +1,102 @@
 import React, { Component } from 'react';
 import './App.scss';
+import downArrow from './Down.svg'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div className="App">
         <header className="App-header">
           <p>
-            I am announcing an artistic project.<br/>
-            The project is unnamed.<br/>
-            Eventually, it is likely to be named.<br/>
-            It is absolutely certain that naming the project will not adequately define it. For the time being, it will simply be referred to as “the project.”<br/>
-            <br/>
-            I have already begun the project. I am simply announcing its existence and declaring the next stage.<br/>
-            It is multi-layered, multi-faceted and takes place over a continuum of time.<br/>
-            More will be shared when appropriate. I am confident that if I am not able to see the project all the way through, others will, or it won't matter.<br/>
-            But don’t worry, I do not plan on ending my work on it any time soon.<br/>
-            I will continue to complete works from prior periods, even as I work on this next one.<br/>
-            <br/>
-            We begin this stage with zen circles.<br/>
+
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            
-          </a>
         </header>
+        <body className="App-body">
+            <div className="App-announce">
+                I am announcing an artistic project.<br/>
+                The project is unnamed.<br/>
+                Eventually, it is likely to be named.<br/>
+                It is certain that naming the project will not adequately define it.<br/>
+                 For the time being, it will simply be referred to as “the project.”<br/>
+                <br/>
+                I have already begun the project. I am simply announcing its existence and declaring the next stage.<br/>
+                It is multi-layered, multi-faceted and takes place over time.<br/>
+                More will be shared when appropriate. I am confident that if I am not able to see the project all the way through, others will, or it won't matter.<br/>
+                But don’t worry, I do not plan on ending my work on it any time soon.<br/>
+                I will continue to complete works from prior periods, even as I work on this next one.<br/>
+                <br/>
+                We begin this stage with zen circles.<br/>
+                <br/>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 91.3" className="App-down">
+                <path d="M300 10.1L294.7 0 150.1 78.3 5.5 0 0 10.1l149.9 81.2.2-.2v.2L300 10.1z" fill="#2f2d30"/>
+            </svg>
+            <div className="App-faq">
+                FAQ<br/>
+                You’re being cryptic intentionally, aren’t you?<br/>
+                Yes. A girl’s got to have some secrets. <br/>
+                <br/>
+                What is a zen circle? <br/>
+                It is a practice from Japan in which one sits, grinds ink, and then tries to draw a perfect circle. It is said that if you succeed, you will be enlightened. Now what enlightenment is, whether the process works, if you must be enlightened to accomplish the task, or if accomplishing it creates enlightenment in the creator, or even if enlightenment itself exists, is really anyone’s opinion… But the practice and the drishti (focus/inward gaze) I find exceptionally beautiful.<br/>
+                <br/>
+                When will you do them?<br/>
+                Soon. Very soon.<br/>
+                <br/>
+                Where will they be made?<br/>
+                Each will be completed at a different location.<br/>
+                <br/>
+                Where will the next one be made?<br/> 
+                The next one to be produced will happen within New York City.<br/>
+                <br/>
+                How many will you make?<br/>
+                111 in this stage. Possibly more.<br/>
+                <br/>
+                Have you made any of them already?<br/>
+                Yes.<br/>
+                <br/>
+                How many?<br/>
+                More than one.<br/>
+                <br/>
+                Will you teach others how to make them?<br/>
+                It really isn’t complicated, but sure.<br/>
+                <br/>
+                What’s to stop me from just making my own and claiming it’s one of yours?<br/>
+                Nothing. I encourage you to do so. It is a good practice to implement.<br/>
+                <br/>
+                What medium(s) will you use?<br/>
+                This will be variable.<br/>
+                <br/>
+                Will copies be made?<br/>
+                I certainly hope so.<br/>
+                <br/>
+                Will I be able to see you complete them?<br/>
+                Maybe some of them, it is doubtful that you will see all of them produced.<br/>
+                <br/>
+                Will they be recorded?<br/>
+                Yes.<br/>
+                <br/>
+                Will those recordings be available for viewing?<br/>
+                Some will be to all. All will be to some.<br/>
+                <br/>
+                Will I be able to acquire a zen circle or one of its copies? 
+                Most likely. I’d be surprised if you couldn’t acquire a share/facsimile/piece/representation of at least one of them.<br/>
+                One will be available to whomsoever provides the most support to this project. One format of support is financial backing. It is not the only format. <br/>
+                I can assure you that earlier stage support of the project will not be forgotten and will definitely be rewarded.<br/>
+                <br/>
+                Will you sell any of your art/tools/possessions to fund this process?<br/>
+                Yes.Everything will be given up when it is clear that I no longer need it for this artistic project, or that providing it to another will make for better use. If you have a desire/interest/offer, please reach out.<br/>
+                <br/>
+                Can I help you do this project?<br/>
+                Yes.<br/>
+                <br/>
+                How?<br/>
+                Well, we’d need to have a conversation or two about that. I only want the kind of support that can be comfortably and freely given.<br/>
+                <br/>
+                Will there be a gallery show of these circles?<br/>
+                For some, yes. For others, it will be quite impossible to share in that manner.<br/>
+            </div>
+        </body>
       </div>
     );
   }
