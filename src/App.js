f87c8cd802373be97f821f88a9787adbfc9ffeaf
Matthias Elliott <komizutama@gmail.com>
2018-10-03 00:24:02 -0400

    text added back and reformatted to react.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
@@ -7,9 +6,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            I am announcing an artistic project.<br/>
+            The project is unnamed.<br/>
+            Eventually, it is likely to be named.<br/>
+            It is absolutely certain that naming the project will not adequately define it. For the time being, it will simply be referred to as “the project.”<br/>
+            <br/>
+            I have already begun the project. I am simply announcing its existence and declaring the next stage.<br/>
+            It is multi-layered, multi-faceted and takes place over a continuum of time.<br/>
+            More will be shared when appropriate. I am confident that if I am not able to see the project all the way through, others will, or it won't matter.<br/>
+            But don’t worry, I do not plan on ending my work on it any time soon.<br/>
+            I will continue to complete works from prior periods, even as I work on this next one.<br/>
+            <br/>
+            We begin this stage with zen circles.<br/>
           </p>
           <a
             className="App-link"
@@ -17,7 +26,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            
           </a>
         </header>
       </div>
